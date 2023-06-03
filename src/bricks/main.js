fdbe8400a31ad844626cdc95300a660685d6bed6
Witelm <taurel@mail.ru>
2023-06-03 08:59:10 +0300

    code

--- src/bricks/main.js
+++ src/bricks/main.js
@@ -1,37 +1,43 @@
 import React from 'react'
 
+function Nav() {
+  return (
+    <nav className="main__nav nav">
+      <div className="nav__logo logo">
+        <img className="logo__image" src="img/logo.png" alt="logo"></img>
+      </div>
+      <div className="nav__burger burger">
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+        <span className="burger__line"></span>
+      </div>
+      <div className="nav__menu menu">
+        <ul className="menu__list">
+          <li className="menu__item">
+            <a href="http://" className="menu__link">
+              Главное
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="http://" className="menu__link">
+              Мой плейлист
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="http://" className="menu__link">
+              Войти
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
 function Main() {
   return (
     <main className="main">
-      <nav className="main__nav nav">
-        <div className="nav__logo logo">
-          <img className="logo__image" src="img/logo.png" alt="logo"></img>
-        </div>
-        <div className="nav__burger burger">
-          <span className="burger__line"></span>
-          <span className="burger__line"></span>
-          <span className="burger__line"></span>
-        </div>
-        <div className="nav__menu menu">
-          <ul className="menu__list">
-            <li className="menu__item">
-              <a href="http://" className="menu__link">
-                Главное
-              </a>
-            </li>
-            <li className="menu__item">
-              <a href="http://" className="menu__link">
-                Мой плейлист
-              </a>
-            </li>
-            <li className="menu__item">
-              <a href="http://" className="menu__link">
-                Войти
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Nav />
       <div className="main__centerblock centerblock">
         <div className="centerblock__search search">
           <svg className="search__svg">
