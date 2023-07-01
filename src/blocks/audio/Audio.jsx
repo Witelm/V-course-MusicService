d5f5c81f56c51052a88286d2d7f14e975b4aa1e2
Witelm <taurel@mail.ru>
2023-07-01 07:40:10 +0300

    add clearInterval for audio.jsx

--- src/blocks/audio/Audio.jsx
+++ src/blocks/audio/Audio.jsx
@@ -13,6 +13,9 @@ export default function Audio({ play, setCompleted }) {
         (audioRef.current.currentTime / audioRef.current.duration) * 100
       )
     }, 200)
+    return () => {
+      clearInterval(TimerId)
+    }
   }, [])
 
   return (
