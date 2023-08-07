63d2ab50dbc6c8398cde20d15f882a69c97d18eb
Witelm <taurel@mail.ru>
2023-08-07 09:19:49 +0300

    registered page

--- src/blocks/Pages/Registration.jsx
+++ src/blocks/Pages/Registration.jsx
@@ -1,23 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import s from './Registration.module.css'
 import { userFunck } from '../../store/actions/thunk/user'
 import { useDispatch, useSelector } from 'react-redux'
-import { store } from '../../store/store'
 
 export const Registration = (props) => {
   const dispatch = useDispatch()
-  const [state, setState] = useState(false)
+  const [isRegistered, setIsRegistered] = useState(false)
 
-  const [stateUser, setUser] = useState('')
-  const [stateEmail, setEmail] = useState('')
-  const [statePass, setPass] = useState('')
-  const [statePassTwo, setPassTwo] = useState('')
+  const [stateUser, setStateUser] = useState('')
+  const [stateEmail, setStateEmail] = useState('')
+  const [statePass, setStatePass] = useState('')
+  const [statePassTwo, setStatePassTwo] = useState('')
 
   const user = useSelector((state) => state.user.username)
+
   const handleState = () => {
-    setState(true)
-    console.log(user)
-    store.getState().user.username ? setState(false) : setState(true)
+    setIsRegistered((prevState) => !prevState)
   }
 
   // const inputUser = {
@@ -26,7 +24,6 @@ export const Registration = (props) => {
   //   email: 'try@mail.ru',
   // }
 
-  // const inputUser = {}
   // const inputUser = {
   //   username: '.as',
   //   password: '.as123454321',
@@ -38,50 +35,33 @@ export const Registration = (props) => {
   //   email: 'qq@mail.ru',
   // }
 
-  const inputUser = {}
-
-  const loginInput = (e) => {
-    inputUser.username = e.target.value
-    setUser(e.target.value)
-  }
-  const passInput = (e) => {
-    inputUser.password = e.target.value
-    setPass(e.target.value)
-  }
-  const emailInput = (e) => {
-    inputUser.email = e.target.value
-    setEmail(e.target.value)
+  const handleLogin = () => {
+    const inputUser = {
+      username: stateUser,
+      password: statePass,
+      email: stateEmail,
+    }
+    dispatch(userFunck(inputUser, 'login'))
+    clearForm()
   }
 
-  const handleFunck = (e) => {
-    const target = e.target.textContent
-    target === 'Войти'
-      ? dispatch(userFunck(inputUser, 'login'))
-      : dispatch(userFunck(inputUser, 'signup'))
-    setState(false)
-    setUser('')
-    setEmail('')
-    setPass('')
+  const handleSignup = () => {
+    const inputUser = {
+      username: stateUser,
+      password: statePass,
+      email: stateEmail,
+    }
+    dispatch(userFunck(inputUser, 'signup'))
+    clearForm()
+    setIsRegistered(false)
   }
 
-  const ComeButton = (
-    <button className={s.btn} onClick={handleFunck}>
-      Войти
-    </button>
-  )
-
-  const RegisterButton = (
-    <button className={`${state ? s.btn : s.btn_reg}`} onClick={handleState}>
-      Зарегистрироваться
-    </button>
-  )
-
-  const SendButton = (
-    <button className={s.btn} onClick={handleFunck}>
-      Зарегистрироваться
-    </button>
-  )
-  const Input = <input className={s.input} placeholder="Повторите пароль" />
+  const clearForm = () => {
+    setStateUser('')
+    setStateEmail('')
+    setStatePass('')
+    setStatePassTwo('')
+  }
 
   return (
     <div className={s.wrapper}>
@@ -91,29 +71,58 @@ export const Registration = (props) => {
             <div>
               <img src="img/Icon.svg" alt="" />
             </div>
-
-            <input
-              className={s.input}
-              placeholder="Логин"
-              value={stateUser}
-              onChange={loginInput}
-            />
+            {isRegistered ? (
+              <>
+                <input
+                  className={s.input}
+                  placeholder="Логин"
+                  value={stateUser}
+                  onChange={(e) => setStateUser(e.target.value)}
+                />
+                <input
+                  className={s.input}
+                  placeholder="email"
+                  value={stateEmail}
+                  onChange={(e) => setStateEmail(e.target.value)}
+                />
+              </>
+            ) : (
+              <input
+                className={s.input}
+                placeholder="Логин (email)"
+                value={stateEmail}
+                onChange={(e) => setStateEmail(e.target.value)}
+              />
+            )}
             <input
               className={s.input}
               placeholder="Пароль"
               value={statePass}
-              onChange={passInput}
+              onChange={(e) => setStatePass(e.target.value)}
             />
 
-            <input
-              className={s.input}
-              placeholder="email"
-              value={stateEmail}
-              onChange={emailInput}
-            />
-
-            {state ? Input : ComeButton}
-            {state ? SendButton : RegisterButton}
+            {isRegistered ? (
+              <>
+                <input
+                  className={s.input}
+                  placeholder="Повторите пароль"
+                  value={statePassTwo}
+                  onChange={(e) => setStatePassTwo(e.target.value)}
+                />
+                <button className={s.btn} onClick={handleSignup}>
+                  Зарегистрироваться
+                </button>
+              </>
+            ) : (
+              <>
+                <button className={s.btn} onClick={handleLogin}>
+                  Войти
+                </button>
+                <button className={s.btn} onClick={handleState}>
+                  Зарегистрироваться
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
